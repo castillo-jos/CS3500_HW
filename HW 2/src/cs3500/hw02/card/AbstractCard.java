d882a3c9b3f06133059b3359b71aa034ff72425f
jcastillo96 <castillo.jos@husky.neu.edu>
2017-05-14 18:30:09 -0400

    primilinary changes to HW2/ added card directory to hold card interface, abstract card class

---
+++ HW 2/src/cs3500/hw02/card/AbstractCard.java
@@ -0,0 +1,8 @@
+package cs3500.hw02.card;
+
+/**
+ * Created by Gus on 5/14/2017.
+ */
+public abstract class AbstractCard implements card {
+
+}
---
+++ HW 2/src/cs3500/hw02/card/card.java
@@ -0,0 +1,10 @@
+package cs3500.hw02.card;
+
+/**
+ * Created by Gus on 5/14/2017.
+ */
+public interface card {
+  String getSuit();
+
+  String getValue();
+}
