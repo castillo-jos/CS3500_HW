97512e11e66eb423388a908ab16ce52a1584ba32
jcastillo96 <castillo.jos@husky.neu.edu>
2017-05-15 12:53:45 -0400

    added bucky tutorials on java arrays

---
+++ Tutorials/Youtube/thenewboston/Java Programming Tutorial - 27 - Introduction to Arrays/src/apples.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Gus on 5/15/2017.
+ */
+public class apples {
+  public static void main(String[] args) {
+    int bucky[] = {2, 4, 5, 7, 9};
+
+    System.out.println(bucky[2]);
+  }
+}
---
+++ Tutorials/Youtube/thenewboston/Java Programming Tutorial - 28 - Creating an Array Table/src/apples.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Gus on 5/15/2017.
+ */
+public class apples {
+  public static void main(String[] args) {
+    System.out.println("Index\tValue");
+    int bucky[] = {32, 12, 18, 54, 2};
+
+    for (int counter = 0; counter < bucky.length; counter++) {
+      System.out.println(counter + "\t\t" + bucky[counter]);
+    }
+  }
+}
